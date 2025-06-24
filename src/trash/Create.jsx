4e5d73b50f48747ce875f24a5c42e6ca23a01087
heislerlemos <heisler.stlano@gmail.com>
2025-06-24 17:04:20 +0100

    adding auto resize columns

--- src/trash/Create.jsx
+++ src/trash/Create.jsx
@@ -1,23 +1,34 @@
-import { FaCheck } from "react-icons/fa6";
+import { FaFileImage } from "react-icons/fa6";
+import { FaHouse } from "react-icons/fa6";
+import { FaTrashCan } from "react-icons/fa6";
+import { FaTruck } from "react-icons/fa6";
+import { FaSquarePhone } from "react-icons/fa6";
 import { FaUser } from "react-icons/fa6";
 
+import "../assets/css/Create.css";
 
 function Create() {
   return (
     <>
-      <div className="title">Crie a localização</div>
+      <div className="title">Cria a localização</div>
       <br />
       <div className="container">
         <div className="columns is-centered">
           <div className="column is-mobile is-half">
             <div className="field">
               <label className="label">Imagem</label>
-              <div className="control">
+
+              <div className="control has-icons-left">
                 <input
                   className="input"
                   type="text"
                   placeholder="Insira o link de uma imagem"
                 />
+                <span className="icon is-small is-left">
+                  <i>
+                    <FaFileImage />
+                  </i>
+                </span>
               </div>
             </div>
 
@@ -30,82 +41,205 @@ function Create() {
                   placeholder="Insira nome da localização ex. Cazenga São Paulo"
                 />
                 <span className="icon is-small is-left">
-                  <i><FaUser /></i>
-                </span>
-                <span className="icon is-small is-right">
-                  <i><FaCheck /></i>
+                  <i>
+                    <FaHouse />
+                  </i>
                 </span>
               </div>
             </div>
 
             <div className="field">
-              <label className="label">Nivel do lixo</label>
+              <label className="label">
+                Quantas recolhas a Elisal faz por dia nesta zona
+              </label>
               <div className="control has-icons-left has-icons-right">
-                <input
-                  className="input is-danger"
-                  type="email"
-                  placeholder="Email input"
-                />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-envelope"></i>
-                </span>
-                <span className="icon is-small is-right">
-                  <i className="fas fa-exclamation-triangle"></i>
-                </span>
-              </div>
-              <p className="help is-danger">This email is invalid</p>
-            </div>
-
-            <div className="field">
-              <label className="label">Subject</label>
-              <div className="control">
                 <div className="select">
                   <select>
-                    <option>Select dropdown</option>
-                    <option>With options</option>
+                    <option>N/A</option>
+                    <option>0</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
                   </select>
                 </div>
+
+                <span className="icon is-small is-left">
+                  <i>
+                    <FaTruck />
+                  </i>
+                </span>
               </div>
             </div>
 
             <div className="field">
-              <label className="label">Message</label>
-              <div className="control">
-                <textarea
-                  className="textarea"
-                  placeholder="Textarea"
-                ></textarea>
+              <label className="label">
+                Quantas postos de lixo tem na zona
+              </label>
+              <div className="control has-icons-left has-icons-right">
+                <input
+                  className="input"
+                  type="number"
+                  placeholder="Insira o numero de posto de lixo que tem nesta zona"
+                  id="posto_de_lixo"
+                />
+                <span className="icon is-small is-left">
+                  <i>
+                    <FaTrashCan />
+                  </i>
+                </span>
               </div>
             </div>
+            <br />
 
             <div className="field">
-              <div className="control">
-                <label className="checkbox">
-                  <input type="checkbox" />I agree to the{" "}
-                  <a href="#">terms and conditions</a>
-                </label>
+              <label className="label">
+                Contacto dos agentes de recolha de lixo domiciliario
+              </label>
+              <div className="control has-icons-left ">
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="Insira o nome do agente numero 1º"
+                />
+                <span className="icon is-small is-left">
+                  <i>
+                    <FaUser />
+                  </i>
+                </span>
               </div>
+              <br />
+              <div className="control has-icons-left has-icons-right ">
+                <input
+                  className="input"
+                  type="number"
+                  placeholder="Insira o numero do agente numero 1º"
+                  id="numero_agente"
+                />
+                <span className="icon is-small is-left">
+                  <i>
+                    <FaSquarePhone />
+                  </i>
+                </span>
+              </div>
+              <br />
+              <div className="control has-icons-left ">
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="Insira o nome do agente numero 2º"
+                />
+                <span className="icon is-small is-left">
+                  <i>
+                    <FaUser />
+                  </i>
+                </span>
+              </div>
+              <br />
+              <div className="control has-icons-left has-icons-right ">
+                <input
+                  className="input"
+                  type="number"
+                  placeholder="Insira o numero do agente numero 2º"
+                  id="numero_agente"
+                />
+                <span className="icon is-small is-left">
+                  <i>
+                    <FaSquarePhone />
+                  </i>
+                </span>
+              </div>
+              <br />
+                            <div className="control has-icons-left ">
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="Insira o nome do agente numero 3º"
+                />
+                <span className="icon is-small is-left">
+                  <i>
+                    <FaUser />
+                  </i>
+                </span>
+              </div>
+              <br />
+              <div className="control has-icons-left has-icons-right ">
+                <input
+                  className="input"
+                  type="number"
+                  placeholder="Insira o numero do agente numero 3º"
+                  id="numero_agente"
+                />
+                <span className="icon is-small is-left">
+                  <i>
+                    <FaSquarePhone />
+                  </i>
+                </span>
+              </div>
+              <br/>
+                            <div className="control has-icons-left ">
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="Insira o nome do agente numero 4º"
+                />
+                <span className="icon is-small is-left">
+                  <i>
+                    <FaUser />
+                  </i>
+                </span>
+              </div>
+              <br />
+              <div className="control has-icons-left has-icons-right ">
+                <input
+                  className="input"
+                  type="number"
+                  placeholder="Insira o numero do agente numero 4º"
+                  id="numero_agente"
+                />
+                <span className="icon is-small is-left">
+                  <i>
+                    <FaSquarePhone />
+                  </i>
+                </span>
+              </div>
+              <br/>
+                            <div className="control has-icons-left ">
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="Insira o nome do agente numero 5º"
+                />
+                <span className="icon is-small is-left">
+                  <i>
+                    <FaUser />
+                  </i>
+                </span>
+              </div>
+              <br />
+              <div className="control has-icons-left has-icons-right ">
+                <input
+                  className="input"
+                  type="number"
+                  placeholder="Insira o numero do agente numero 5º"
+                  id="numero_agente"
+                />
+                <span className="icon is-small is-left">
+                  <i>
+                    <FaSquarePhone />
+                  </i>
+                </span>
+              </div>
+              <br/>
             </div>
 
-            <div className="field">
+            <div className="field is-grouped is-centered">
               <div className="control">
-                <label className="radio">
-                  <input type="radio" name="question" />
-                  Yes
-                </label>
-                <label className="radio">
-                  <input type="radio" name="question" />
-                  No
-                </label>
-              </div>
-            </div>
-
-            <div className="field is-grouped">
-              <div className="control">
-                <button className="button is-link">Submit</button>
+                <button className="button  is-link">Submeter</button>
               </div>
               <div className="control">
-                <button className="button is-link is-light">Cancel</button>
+                <button className="button  is-link is-light">Cancelar</button>
               </div>
             </div>
           </div>
--- src/trash/View.jsx
+++ src/trash/View.jsx
@@ -1,12 +1,16 @@
 import "bulma/css/bulma.css";
 import "../assets/css/View.css";
+import $ from "jquery";
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-const View= () => {
+
+
+const View = () => {
   return (
     <>
       <div class="container">
-        <div class="columns is-multiline is-mobile">
-          <div class="column is-auto">
+        <div class="columns is-multiline is-mobile is-centered">
+          <div className="column is-auto">
             <div class="card">
               <div class="card-image">
                 <figure class="image is-4by3">
@@ -34,234 +38,29 @@ const View= () => {
               </div>
             </div>
           </div>
-          <div class="column is-auto">
-            <div class="card">
-              <div class="card-image">
-                <figure class="image is-4by3">
-                  <img
-                    src="https://www.verangola.net/va/images/cms-image-000030708.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
-              </div>
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-content">
-                    <p class="title is-4">6 Yard Skip</p>
-                    <p class="subtitle is-6">14 day hire period</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  <div id="money">£305</div>
-                  <br />
-                  <button class="button is-dark is-large">
-                    Select This Skip{" "}
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column is-auto">
-            <div class="card">
-              <div class="card-image">
-                <figure class="image is-4by3">
-                  <img
-                    src="https://www.verangola.net/va/images/cms-image-000030708.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
-              </div>
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-content">
-                    <p class="title is-4">8 Yard Skip</p>
-                    <p class="subtitle is-6">14 day hire period</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  <div id="money">£375</div>
-                  <br />
-                  <button class="button is-dark is-large">
-                    Select This Skip{" "}
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column is-auto">
-            <div class="card">
-              <div class="card-image">
-                <figure class="image is-4by3">
-                  <img
-                    src="https://www.verangola.net/va/images/cms-image-000030708.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
-              </div>
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-content">
-                    <p class="title is-4">10 Yard Skip</p>
-                    <p class="subtitle is-6">14 day hire period</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  <div id="money">£400</div>
-                  <br />
-                  <button class="button is-dark is-large">
-                    Select This Skip{" "}
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column is-auto">
-            <div class="card">
-              <div class="card-image">
-                <figure class="image is-4by3">
-                  <img
-                    src="https://www.verangola.net/va/images/cms-image-000030708.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
-              </div>
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-content">
-                    <p class="title is-4">12 Yard Skip</p>
-                    <p class="subtitle is-6">14 day hire period</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  <div id="money">£439</div>
-                  <br />
-                  <button class="button is-dark is-large">
-                    Select This Skip{" "}
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column is-auto">
-            <div class="card">
-              <div class="card-image">
-                <figure class="image is-4by3">
-                  <img
-                    src="https://www.verangola.net/va/images/cms-image-000030708.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
-              </div>
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-content">
-                    <p class="title is-4">14 Yard Skip</p>
-                    <p class="subtitle is-6">14 day hire period</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  <div id="money">£470</div>
-                  <br />
-                  <button class="button is-dark is-large">
-                    Select This Skip{" "}
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column is-auto">
-            <div class="card">
-              <div class="card-image">
-                <figure class="image is-4by3">
-                  <img
-                    src="https://www.verangola.net/va/images/cms-image-000030708.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
-              </div>
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-content">
-                    <p class="title is-4">16 Yard Skip</p>
-                    <p class="subtitle is-6">14 day hire period</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  <div id="money">£496</div>
-                  <br />
-                  <button class="button is-dark is-large">
-                    Select This Skip{" "}
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column is-auto">
-            <div class="card">
-              <div class="card-image">
-                <figure class="image is-4by3">
-                  <img
-                    src="https://www.verangola.net/va/images/cms-image-000030708.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
-              </div>
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-content">
-                    <p class="title is-4">20 Yard Skip</p>
-                    <p class="subtitle is-6">14 day hire period</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  <div id="money">£248</div>
-                  <br />
-                  <button class="button is-dark is-large">
-                    Select This Skip{" "}
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column is-auto">
-            <div class="card">
-              <div class="card-image">
-                <figure class="image is-4by3">
-                  <img
-                    src="https://www.verangola.net/va/images/cms-image-000030708.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
-              </div>
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-content">
-                    <p class="title is-4">40 Yard Skip</p>
-                    <p class="subtitle is-6">14 day hire period</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  <div id="money">£992</div>
-                  <br />
-                  <button class="button is-dark is-large">
-                    Select This Skip{" "}
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
+          
         </div>
       </div>
     </>
+    
   );
 };
+
+function resize() {
+  if ($(window).width() < 700) {
+    $('.column').addClass('is-auto');
+  } else {
+    $('.column').removeClass('is-auto');
+  }
+}
+
+$(document).ready(function() {
+  resize();
+});
+
+$(window).on('resize', function() {
+  resize();
+});
+
 
 export default View;
